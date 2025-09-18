fb2bb8e7c7959ba480fed75a6345606c53d9447e
X1erxes <wei.xerxes@gmail.com>
2025-09-18 11:36:41 +0800

    first_cmt

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def read_file(filename):
+    pass
+
+
+def main():
+    if len(sys.argv) != 4:
+        print(" ‰»Î¥ÌŒÛ")
+        return
+
+    original_file = sys.argv[1]
+    plagiarized_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    original_text = read_file(original_file)
+    if original_text is None:
+        return
+
+    plagiarized_text = read_file(plagiarized_file)
+    if plagiarized_text is None:
+        return
+
+
+if __name__ == "__main__":
+    main()
